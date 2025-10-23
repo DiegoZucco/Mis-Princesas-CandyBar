fe0e12e9981ede7854966d546ee5460d40230cab
Diego Zuccolilli <Dzuccolilli@gmail.com>
2025-10-23 18:07:36 -0300

    arreglo el deploy para que funcione en github pages y en despliegues locales

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Mis-Princesas-CandyBar/', // <- necesario para GitHub Pages
   plugins: [react()],
   base: './https://github.com/DiegoZucco/Mis-Princesas-CandyBar', // <- necesario para despliegues locales o en servidores personalizados
 })
