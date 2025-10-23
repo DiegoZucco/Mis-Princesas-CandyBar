30840b630ba547dd2d46222530c7977377a5e0d7
Diego Zuccolilli <Dzuccolilli@gmail.com>
2025-10-23 18:24:57 -0300

    otro arreglo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './https://github.com/DiegoZucco/Mis-Princesas-CandyBar', // <- necesario para despliegues locales o en servidores personalizados
+  base: "/Mis-Princesas-CandyBar/",
 })
