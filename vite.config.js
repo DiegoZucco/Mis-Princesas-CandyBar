477f0866c9d746bb1b71b52e0f9720e78ae0848c
Diego Zuccolilli <Dzuccolilli@gmail.com>
2025-10-23 18:04:31 -0300

    intento de publicacion a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Mis-Princesas-CandyBar/', // <- necesario para GitHub Pages
   plugins: [react()],
+  base: './https://github.com/DiegoZucco/Mis-Princesas-CandyBar', // <- necesario para despliegues locales o en servidores personalizados
 })
